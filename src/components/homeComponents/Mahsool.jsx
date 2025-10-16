16736aa0ba8c25a4c941e8a10ab90d15005677c2
AmirK <amirkhoramdelaval@gmail.com>
2025-10-16 13:35:20 +0330

    syncing tailwind with mui + fixing slider bugs

--- src/components/homeComponents/Mahsool.jsx
+++ src/components/homeComponents/Mahsool.jsx
@@ -1,18 +1,12 @@
-import React from "react";
-import batman from "../images/batMan.png";
-
-const Mahsool = ({ price, title }) => {
+const Mahsool = ({ price, title, image }) => {
   return (
-    <div>
-      <div className="min-w-[80%] md:min-w-[40%]">
-        <div
-          style={{ backgroundImage: "url(" + { batman } + ")" }}
-          className="h-80 w-full snap-center rounded-xl bg-cover bg-center shadow-md shadow-gray-500/30"
-        ></div>
-        <div>{title}</div>
-        <div>{price}</div>
-      </div>
-      ;
+    <div className="min-w-[80%] md:min-w-[40%]">
+      <div
+        style={{ backgroundImage: `url(${image})` }}
+        className="h-80 w-full snap-center rounded-xl bg-cover bg-center shadow-md shadow-gray-500/30"
+      ></div>
+      <div>{title}</div>
+      <div>{price}</div>
     </div>
   );
 };
--- src/theme.js
+++ src/theme.js
@@ -1,76 +1,68 @@
-import { createTheme } from "@mui/material/styles";
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
 
-const muiTheme = createTheme({
+const muiTheme = extendTheme({
+  cssVarPrefix: "mui",
   direction: "rtl",
   palette: {
     mode: "light",
     primary: { 
-      main: "#dc2655" ,
-      main50: "#fdf2f6",
-      main100: "#fce7ef",
-      main200: "#fbcfe0",
-      main300: "#faa7c5",
-      main400: "#f5719e",
-      main500: "#ef5a88",
-      main700: "#bf173e",
-      main800: "#9e1634",
-      main900: "#84172f",
-      main950: "#510616",
+      main: "#dc2655",
+      light: "#f5719e",
+      dark: "#9e1634",
+      shades: {
+        50: "#fdf2f6",
+        100: "#fce7ef",
+        200: "#fbcfe0",
+        300: "#faa7c5",
+        400: "#f5719e",
+        500: "#ef5a88",
+        700: "#bf173e",
+        800: "#9e1634",
+        900: "#84172f",
+        950: "#510616",
+      },
     },
-
-    common:{
+    common: {
       black: "#151617",
       white: "#e1e2e3",
     },
-    success:{
+    success: {
       main: "#00a478",
       light: "#00ba88",
       dark: "#008364",
     },
-    warning:{
+    warning: {
       main: "#f3a62c",
       light: "#f4b740",
       dark: "#ec8514",
     },
-    error:{
+    error: {
       main: "#ed2e2e",
       light: "#f87171",
       dark: "#dc2626",
     },
-    gray:{
-       50: "#f0f1f1" ,
-       100: "#e1e2e3" ,
-       200: "#d1d4d4 ",
-       300: "#c2c5c6" ,
-       400: "#a4a9aa" ,
-       500: "#858c8d" ,
-       600: "#676f71" ,
-       700: "#52595a" ,
-       800: "#3e4344" , 
-       900: "#292c2d" ,
-       950: "#151617",
+    grey: {
+      50: "#f0f1f1",
+      100: "#e1e2e3",
+      200: "#d1d4d4",
+      300: "#c2c5c6",
+      400: "#a4a9aa",
+      500: "#858c8d",
+      600: "#676f71",
+      700: "#52595a",
+      800: "#3e4344",
+      900: "#292c2d",
+      950: "#151617",
     },
   },
   typography: {
     fontFamily: "var(--font-sans)",
-    h1:{
-      fontSize: "48px",
-    },
-     h2:{
-      fontSize: "40px",
-    },
-     h3:{
-      fontSize: "32px",
-    },
-     h4:{
-      fontSize: "",
-    },
-     h5:{
-      fontSize: "",
-    },
-     h6:{
-      fontSize: "",
-    },
+    h1: { fontSize: "48px" },
+    h2: { fontSize: "40px" },
+    h3: { fontSize: "32px" },
+    h4: { fontSize: "24px" },
+    h5: { fontSize: "20px" },
+    h6: { fontSize: "16px" },
   },
 });
 
